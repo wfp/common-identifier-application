9b238fa5fdd22477f341bbe9acad9c25a00b3bce
Gyula Laszlo <gyula.laszlo.gm@gmail.com>
2024-09-09 12:28:57 +0200

    update build script for submodules

--- build-app.sh
+++ build-app.sh
@@ -1,41 +1,17 @@
-# TODO: these should be injected from the build pipeline
-ALGORITHM_WRAPPER_REPO=...
-ALGORITHM_NWS_REPO=...
-ALGORITHM_GOS_REPO=...
-
-BUILD_OUTPUT_DIR=./_build
-
+# Update the algo repos
+git submodule init
 git submodule update
 
-# NWS
-# ===
+# Build the renderer
+(cd src/renderer && npm install && npm run build)
 
+# Install dependencies for building
+npm install
 
-node tools/activate-algo.js algo-uscadi
+# Call per-platform builders
 
-# Build the executable for windows and mac
-# NOTE: the "arch=x64" for windows is needed on mac to have it build the proper executable
-echo "BUILDING FOR WINDOWS"
-npx electron-forge make --arch=x64 --platform=win32
+echo "Building WINDOWS version"
+bash ./build-win
 
-echo "BUILDING FOR MACOS"
-npx electron-forge make --platform=darwin
-
-cp -r out/ui-win32-x64    $BUILD_OUTPUT_DIR/nws/ui-win32-x64
-cp -r out/ui-darwin-arm64 $BUILD_OUTPUT_DIR/nws/ui-darwin-arm64
-
-# GOS
-# ===
-
-node tools/activate-algo.js algo-gos
-
-# Build the executable for windows and mac
-# NOTE: the "arch=x64" for windows is needed on mac to have it build the proper executable
-echo "BUILDING FOR WINDOWS"
-npx electron-forge make --arch=x64 --platform=win32
-
-echo "BUILDING FOR MACOS"
-npx electron-forge make --platform=darwin
-
-cp -r out/ui-win32-x64    $BUILD_OUTPUT_DIR/gos/ui-win32-x64
-cp -r out/ui-darwin-arm64 $BUILD_OUTPUT_DIR/gos/ui-darwin-arm64
+echo "Building MAC version"
+bash ./build-mac
