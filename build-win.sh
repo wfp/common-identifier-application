1e791f405d6f411adffb058889a8dd5661a3aaad
Gyula Laszlo <gyula.laszlo.gm@gmail.com>
2024-09-26 15:16:59 +0200

    update build script for windows

--- build-win.sh
+++ build-win.sh
@@ -2,11 +2,11 @@
 echo "---------- NWS -----------------"
 node tools/activate-algo.js algo-uscadi
 npx electron-forge make --arch=x64 --platform=win32
-mv out/ui-win32-x64 out/ui-win32-x64-nws
-(cd out && zip -r ui-win32-x64-nws ui-win32-x64-nws)
+mv out/commonid-tool-win32-x64 out/commonid-tool-win32-x64-nws
+(cd out && zip -r commonid-tool-win32-x64-nws commonid-tool-win32-x64-nws)
 
 echo "---------- GOS -----------------"
 node tools/activate-algo.js algo-gos
 npx electron-forge make --arch=x64 --platform=win32
-mv out/ui-win32-x64 out/ui-win32-x64-gos
-(cd out && zip -r ui-win32-x64-gos ui-win32-x64-gos)
+mv out/commonid-tool-win32-x64 out/commonid-tool-win32-x64-gos
+(cd out && zip -r commonid-tool-win32-x64-gos commonid-tool-win32-x64-gos)
