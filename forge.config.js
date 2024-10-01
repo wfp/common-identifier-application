0061d62dda76ee583bc9bfeaa321ba96a341c5cf
Gyula Laszlo <gyula.laszlo.gm@gmail.com>
2024-10-01 14:03:09 +0200

    add unsigned zip output to build pipeline

--- forge.config.js
+++ forge.config.js
@@ -43,7 +43,7 @@ module.exports = {
     },
     {
       name: '@electron-forge/maker-zip',
-      platforms: ['darwin'],
+      // platforms: ['darwin'],
     },
     // {
     //   name: '@electron-forge/maker-deb',
