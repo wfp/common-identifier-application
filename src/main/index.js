439f6225c486e3ab2d8ac8f3a9c361dd46eb5ada
Gyula Laszlo <gyula.laszlo.gm@gmail.com>
2024-10-04 15:03:23 +0200

    attempt to create shortcuts using Node libraries

--- src/main/index.js
+++ src/main/index.js
@@ -17,9 +17,101 @@ const processFile = require('./ipc-handlers/processFile');
 const preProcessFileOpenDialog = require('./ipc-handlers/preProcessFileOpenDialog');
 const removeUserConfig = require('./ipc-handlers/removeUserConfig');
 
+const createDesktopShortcut = require('create-desktop-shortcuts');
 
-// run this as early in the main process as possible
-if (require('electron-squirrel-startup')) app.quit();
+// INITIAL SQUIRELL EVENT HANDLING
+// -------------------------------
+
+
+
+// this should be placed at top of main.js to handle setup events quickly
+if (handleSquirrelEvent()) {
+    // squirrel event handled and app will exit in 1000ms, so don't do anything else
+    return;
+}
+
+function handleSquirrelEvent() {
+
+    if (process.argv.length === 1) {
+      return false;
+    }
+
+    const ChildProcess = require('child_process');
+    const path = require('path');
+
+    const appFolder = path.resolve(process.execPath, '..');
+    const rootAtomFolder = path.resolve(appFolder, '..');
+    const updateDotExe = path.resolve(path.join(rootAtomFolder, 'Update.exe'));
+    const exeName = path.basename(process.execPath);
+
+    const spawn = function(command, args) {
+      let spawnedProcess, error;
+
+      try {
+        spawnedProcess = ChildProcess.spawn(command, args, {detached: true});
+      } catch (error) {}
+
+      return spawnedProcess;
+    };
+
+    const spawnUpdate = function(args) {
+      return spawn(updateDotExe, args);
+    };
+
+    const squirrelEvent = process.argv[1];
+    switch (squirrelEvent) {
+      case '--squirrel-install':
+      case '--squirrel-updated':
+        // Optionally do things such as:
+        // - Add your .exe to the PATH
+        // - Write to the registry for things like file associations and
+        //   explorer context menus
+        const shortcutsCreated = createDesktopShortcut({
+            // OPTIONAL: defaults to true
+            onlyCurrentOS: true,
+            // OPTIONAL: defaults to true
+            verbose: true,
+            windows: {
+                filePath: exePath,
+                outputPath: appFolder,
+                name: 'Building Blocks CommonID Tool',
+                comment: 'Start the CommonID Tool',
+                // icon: 'C:\\path\\to\\file.ico',
+                // arguments: '--my-argument -f \'other stuff\'',
+                // windowMode: 'normal',
+                // hotkey: 'ALT+CTRL+F'
+            },
+        })
+
+        // // Install desktop and start menu shortcuts
+        // spawnUpdate(['--createShortcut', exeName]);
+
+        // setTimeout(app.quit, 1000);
+        return true;
+
+      case '--squirrel-uninstall':
+        // Undo anything you did in the --squirrel-install and
+        // --squirrel-updated handlers
+
+        // Remove desktop and start menu shortcuts
+        // spawnUpdate(['--removeShortcut', exeName]);
+
+        // setTimeout(app.quit, 1000);
+        return true;
+
+      case '--squirrel-obsolete':
+        // This is called on the outgoing version of your app before
+        // we update to the new version - it's the opposite of
+        // --squirrel-updated
+
+        app.quit();
+        return true;
+    }
+}
+
+
+
+
 
 
 
