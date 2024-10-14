3221ad087369031ebf447aa4758ad1d8b2a9c026
Gyula Laszlo <gyula.laszlo.gm@gmail.com>
2024-10-14 11:59:32 +0200

    added GPL v3 license

--- src/renderer/src/screens/Boot.jsx
+++ src/renderer/src/screens/Boot.jsx
@@ -1,3 +1,19 @@
+/*
+ * This file is part of Building Blocks CommonID Tool
+ * Copyright (c) 2024 WFP
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 
 function Boot() {
     return (<div className="Boot appScreen progressIndicator">
--- src/renderer/src/screens/ValidationSuccess.jsx
+++ src/renderer/src/screens/ValidationSuccess.jsx
@@ -1,3 +1,20 @@
+/*
+ * This file is part of Building Blocks CommonID Tool
+ * Copyright (c) 2024 WFP
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 import FileInfo from "../components/FileInfo";
 import OpenFileRegion from "../components/OpenFileRegion";
 import SheetTabs from "../components/SheetTabs";
@@ -30,7 +47,7 @@ function ValidationSuccess({config, inputData, inputFilePath, isMappingDocument}
                 <div className="validationState">
                     Validation finished. No errors encountered.
                     <div className="help">
-                        { 
+                        {
                             isMappingDocument
                                 ? "Valid mapping file."
                                 : "Valid intended assistance file."
