b96b94032a066930e77f9bbfe2ca6acf3fdbce4c
Dean Ford <dean.ford@wfp.org>
2024-10-31 15:37:28 +0300

    refactor pipeline to support new build process

--- jest.ci.config.js
+++ jest.ci.config.js
@@ -1,21 +1,37 @@
-const config = {
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+export default {
+    // [...]
+    extensionsToTreatAsEsm: ['.ts'],
+    moduleFileExtensions: [ "ts", "js" ],
+    moduleNameMapper: {
+      '^(\\.{1,2}/.*)\\.js$': '$1',
+    },
+    transformIgnorePatterns: ['^.+\\.js$'],
+    transform: {
+      // '^.+\\.[tj]sx?$' to process ts,js,tsx,jsx with `ts-jest`
+      // '^.+\\.m?[tj]sx?$' to process ts,js,tsx,jsx,mts,mjs,mtsx,mjsx with `ts-jest`
+      '^.+\\.tsx?$': [
+        'ts-jest',
+        {
+          useESM: true,
+        },
+      ],
+    },
     // reporters
     reporters: [
-        'default',
-        [
-            'jest-junit',
-            {
-                outputDirectory: './coverage/test-report',
-                outputName: 'test-report.xml',
-            },
-        ],
+      'default',
+      [
+        'jest-junit',
+        {
+          outputDirectory: './coverage/test-report',
+          outputName: 'test-report.xml',
+        },
+      ],
     ],
     // coverage directory
     coverageDirectory: './coverage',
     collectCoverage: true,
     coverageReporters: ["cobertura", "lcov", "html"],
-
+  
     verbose: true,
-};
-
-module.exports = config;
+  }
