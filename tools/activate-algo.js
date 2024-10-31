7a406a67324cace4172b998956347dca025d26d6
Dean Ford <dean.ford@wfp.org>
2024-10-31 15:37:05 +0300

    support algo-specific override of renderer styles + title

--- tools/activate-algo.js
+++ tools/activate-algo.js
@@ -1,61 +1,79 @@
-const fs = require('node:fs');
-const path = require('node:path');
-const toml = require('toml');
-const { program } = require('commander');
+import { writeFileSync, copyFileSync, existsSync, readFileSync, appendFileSync } from 'node:fs';
+import { join, dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { parse as htmlParse } from 'node-html-parser';
+import { program } from 'commander';
 
-program
-    .argument('<algo>', 'The name of the directory inside "src/main/" to activate')
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+program.argument('<algo>', 'The name of the directory inside "src/main/" to activate');
+program.argument('<region>', 'The region suffix to append to rendered title components.');
 
 program.parse();
 
-const options = program.opts();
+const ALGO_NAME = program.args[0];
+const ALGO_REGION = program.args[1];
 
-// APP STARTS HERE
-// ---------------
+const SRC_MAIN_DIR = join(__dirname, "..", "src", "main");
+const ACTIVE_ALGORITHM_FILE = join(SRC_MAIN_DIR, "active_algorithm.ts");
+const BACKUP_CONFIG_TARGET_PATH = join(SRC_MAIN_DIR, "config.backup.toml");
 
-// the 'src/main' directory
-const SRC_MAIN_DIR = path.join(__dirname, "..", "src", "main");
+const ALGO_DIR = join(SRC_MAIN_DIR, ALGO_NAME);
 
-// the active algorithm file
-const ACTIVE_ALGORITHM_FILE = path.join(SRC_MAIN_DIR, "active_algorithm.js");
-
-// the backup config file used for packaging
-const BACKUP_CONFIG_TARGET_PATH = path.join(SRC_MAIN_DIR, "config.backup.toml");
-
-// re-create the 'active_algorithm' file
-// fs.writeFileSync()
+const RENDERER_DIR = join(__dirname, "..", "src", "renderer");
+const RENDERER_CSS_PATH = join(RENDERER_DIR, "dist", "assets", "index.css");
+const OVERRIDE_CSS_PATH = join(ALGO_DIR, "config", "override.css");
+const RENDERER_HTML_PATH = join(RENDERER_DIR, "renderer.html");
 
 
-const algoName = program.args[0];
+console.log("Activating algorithm:", ALGO_NAME);
 
-console.log("Activating algoirhtm:", algoName);
+function writeActiveAlgorithm() {
+    console.log("Generating", ACTIVE_ALGORITHM_FILE);
 
-// WRITE ACTIVE ALGORITHM
-// ----------------------
+    const ACTIVE_ALGO_CONTENTS = `
+    // THIS FILE IS AUTO-GENERATED, YOUR EDITS MAY BE OVERWRITTEN DURING BUILD
+    export { REGION, makeHasher } from './${ALGO_NAME}/index.js';
+    `;
 
-console.log("Generating", ACTIVE_ALGORITHM_FILE);
+    writeFileSync(ACTIVE_ALGORITHM_FILE, ACTIVE_ALGO_CONTENTS, 'utf-8');
+}
 
-const ACTIVE_ALGO_CONTENTS = `
-// THIS FILE IS AUTO-GENERATED, YOUR EDITS MAY BE OVERWRITTEN DURING BUILD
-module.exports = require('./${algoName}');
-`;
+function copyBackupConfig() {
+    // the algorithm directory
+    const algoDir = join(SRC_MAIN_DIR, ALGO_NAME);
 
-fs.writeFileSync(ACTIVE_ALGORITHM_FILE, ACTIVE_ALGO_CONTENTS, 'utf-8');
+    const backupConfigSource = join(algoDir, 'config', 'config.backup.toml');
 
-// COPY BACKUP CONFIG
-// ------------------
+    console.log("Copying backup config from", backupConfigSource );
+    console.log("                        to", BACKUP_CONFIG_TARGET_PATH );
 
-// the algorithm directory
-const algoDir = path.join(SRC_MAIN_DIR, algoName);
+    copyFileSync(backupConfigSource, BACKUP_CONFIG_TARGET_PATH);
+}
 
-const backupConfigSource = path.join(algoDir, 'config', 'config.backup.toml');
+// TODO: actually parse the CSS AST to make sure overrides make sense
+function updateRenderedComponents() {
+    if (!existsSync(OVERRIDE_CSS_PATH)) {
+        console.warn("WARNING: No css overrides present for selected algorithm, keeping index.css unchanged");
+        return
+    }
+    const overrideCss = readFileSync(OVERRIDE_CSS_PATH, "utf-8");
+    // trim out file comment header
+    const cssDeclarations = overrideCss.split("*/")[1].trim();
+    if (cssDeclarations.length === 0) return
+    const sep = "\n/* ==== OVERRIDES ==== */\n"
+    appendFileSync(RENDERER_CSS_PATH, sep + cssDeclarations);
+}
 
-console.log("Copying backup config from", backupConfigSource );
-console.log("                        to", BACKUP_CONFIG_TARGET_PATH );
+function updateRenderedTitle() {
+    const indexHtml = readFileSync(RENDERER_HTML_PATH, "utf-8");
+    const root = htmlParse(indexHtml);
+    const title = root.querySelector('title');
+    title.set_content(`Common Identifier Application - ${ALGO_REGION}`)
+    writeFileSync(RENDERER_HTML_PATH, root.toString())
+}
 
-fs.copyFileSync(backupConfigSource, BACKUP_CONFIG_TARGET_PATH);
-
-// const config = attemptToReadTOMLData(configFile, CONFIG_FILE_ENCODING);
-// const hash = generateConfigHash(config);
-
-// console.log("HASH:", hash);
+writeActiveAlgorithm()
+copyBackupConfig()
+updateRenderedComponents()
+updateRenderedTitle()
