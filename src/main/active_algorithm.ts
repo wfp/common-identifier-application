0320add112e8149822a8fe94b10705440ffc4240
Dean Ford <dean.ford@wfp.org>
2024-11-07 08:20:06 +0300

    [gos] prod config

--- src/main/active_algorithm.ts
+++ src/main/active_algorithm.ts
@@ -1,4 +1,3 @@
-
-    // THIS FILE IS AUTO-GENERATED, YOUR EDITS MAY BE OVERWRITTEN DURING BUILD
-    export { REGION, makeHasher } from './algo-uscadi/index.js';
-    
+// THIS FILE SHOULD NOT BE UPDATED MANUALLY
+// Use the tools/activate-algorithm.js <algo> <region> to populate this file.
+throw new Error("Run tools/activate-algorithm.js <algo-name> <region-name> to populate this file.")
