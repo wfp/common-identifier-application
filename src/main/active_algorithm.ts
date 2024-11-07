f9709708569606cfe8ae9696d0f566da2841f366
Dean Ford <dean.ford@wfp.org>
2024-11-07 09:15:11 +0300

    revert change

--- src/main/active_algorithm.ts
+++ src/main/active_algorithm.ts
@@ -1,3 +1,4 @@
-// THIS FILE SHOULD NOT BE UPDATED MANUALLY
-// Use the tools/activate-algorithm.js <algo> <region> to populate this file.
-throw new Error("Run tools/activate-algorithm.js <algo-name> <region-name> to populate this file.")
+
+    // THIS FILE IS AUTO-GENERATED, YOUR EDITS MAY BE OVERWRITTEN DURING BUILD
+    export { REGION, makeHasher } from './algo-gos/index.js';
+    
