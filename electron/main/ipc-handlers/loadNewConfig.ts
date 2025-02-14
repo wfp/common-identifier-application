adf5442328d325b528708146c921c74019942767
Dean Ford <66484815+dem1fo@users.noreply.github.com>
2025-02-14 10:35:02 +0300

    license header

--- electron/main/ipc-handlers/loadNewConfig.ts
+++ electron/main/ipc-handlers/loadNewConfig.ts
@@ -1,19 +1,18 @@
-/*
- * This file is part of Building Blocks CommonID Tool
- * Copyright (c) 2024 WFP
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, version 3.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
- */
+// Common Identifier Application
+// Copyright (C) 2024 World Food Programme
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import { dialog } from 'electron';
 import type { ConfigStore } from 'common-identifier-algorithm-shared';
--- tools/config-signature.js
+++ tools/config-signature.js
@@ -1,3 +1,18 @@
+// Common Identifier Application
+// Copyright (C) 2024 World Food Programme
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // COMMAND-LINE WRAPPER TO GENERATE SIGNATURES FOR CONFIG FILES
 import { program as programme } from 'commander';
 import { generateConfigHash, attemptToReadTOMLData, validateConfig  } from 'common-identifier-algorithm-shared';
